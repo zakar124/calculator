ddca8c4408181752c9c17928253d617cd14a2640
Jill Zakarian <JillianZakarian@gmail.com>
2022-09-20 16:12:27 -0400

    add buttons to index.html & update styles.css

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 
 let divide = (a, b) => a / b;
+
+
+let operate = function (operator, num1, num2){
+    return operator
+};
+
+operate(add);
+console.log(operate(add));
+
