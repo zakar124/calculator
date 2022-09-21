51c3e2fa4e4b5de81339e9dcce142af50559ed4e
Jill Zakarian <JillianZakarian@gmail.com>
2022-09-21 14:31:06 -0400

    add values to buttons in index.html

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const display = document.querySelector(".display");
+
 let add = (a, b) => a + b;
 
 let subtract = (a, b) => a - b;
@@ -6,9 +8,32 @@ let multiply = (a, b) => a * b;
 
 let divide = (a, b) => a / b;
 
-let operate = function (operator, num1, num2) {
-  return operator;
+// let operate = function (operator, num1, num2) {
+
+// };
+
+let operator = function (operator1, a, b) {
+  switch (operator1) {
+    case "add":
+      display.textContent = add(a, b);
+      break;
+
+    case "subtract":
+      display.textContent = subtract(a, b);
+      break;
+
+    case "multiply":
+      display.textContent = multiply(a, b);
+      break;
+
+    case "divide":
+      display.textContent = divide(a, b);
+      break;
+
+    default:
+      display.textContent = "No value found";
+  }
 };
 
-operate(add);
-console.log(operate(add));
+operator("divide", 10, 2);
+operator("multiply", 10, 2);
